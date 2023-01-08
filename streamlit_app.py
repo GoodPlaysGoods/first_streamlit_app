d897792659362e1cc094538bb0ea97da724a4e71
GoodPlaysGoods <goodplaystreamers@gmail.com>
2023-01-07 23:28:40 -0500

    ?

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,12 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruit?:", list(my_fruit_list.index))
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index))
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
@@ -24,11 +23,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/fruit_choice")
 
 
 # write your own comment -what does the next line do?
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
