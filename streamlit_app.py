9234c2e7781b6ed6a9349f8463a11c290abd3baf
GoodPlaysGoods <goodplaystreamers@gmail.com>
2023-01-08 23:59:10 -0500

    ?

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-streamlit.stop()
+
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -30,6 +30,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/'fruit_choi
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
